17b681a62759f5e5f42dbcb5e095b00e03f8c293
vadimb <vadim.barylo@intapp.com>
2018-05-11 00:13:10 +0300

    add test process

--- generators/app/index.ts
+++ generators/app/index.ts
@@ -5,6 +5,8 @@ import {xmiClass} from "../../src/entities/xmiClass";
 import {xmiComponent} from "../../src/entities/xmiComponent";
 import {xmiPackage} from "../../src/entities/xmiPackage";
 import {default as chalk} from "chalk";
+import {xmiCollaboration} from "../../src/entities/xmiCollaboration";
+import {xmiActor} from "../../src/entities/xmiActor";
 
 const Generator = require('yeoman-generator');
 const yosay = require('yosay');
@@ -13,7 +15,6 @@ const parseString = require('xml2js').parseString;
 
 export class XmiGenerator extends Generator {
     prompting() {
-
         this.log(yosay(`Welcome to the top-notch ${chalk.red('generator-xmi')} generator!`));
     }
 
@@ -24,22 +25,33 @@ export class XmiGenerator extends Generator {
 
             this.log(chalk.green('Model'));
             this.log(treeify.asTree(parser.toConsole(), true, true));
+
+            this.log(chalk.green('Generate'));
+            this._generate(null, parser.packge);
         });
     }
 
-    _generate(path: string, pkg: any) {
+    _generate(path: string | null, pkg: any) {
         path = path || 'dist';
-        pkg = pkg || this.packge;
 
         pkg.children.forEach((x: any) => {
-            if(x instanceof xmiClass || x instanceof xmiComponent) {
-                this.fs.copyTpl(
-                    this.templatePath('xmiClass.ejs'),
-                    this.destinationPath(`${path}/${x.name}.js`), x);
+            if(x instanceof xmiActor) {
+                this.fs.copyTpl(this.templatePath('xmiActor.ejs'), this.destinationPath(`${path}/components/${x.name}.ts`), x);
             }
 
+            if(x instanceof xmiClass || x instanceof xmiComponent) {
+                this.fs.copyTpl(this.templatePath('xmiInterface.ejs'), this.destinationPath(`${path}/contracts/${x.name}.ts`), x);
+                this.fs.copyTpl(this.templatePath('xmiClass.ejs'), this.destinationPath(`${path}/components/${x.name}.ts`), x);
+            }
+
+            if(x instanceof xmiCollaboration) {
+                this.fs.copyTpl(this.templatePath('xmiCollaboration.ejs'), this.destinationPath(`${path}/process/${x.name}.ts`), x);
+                this.fs.copyTpl(this.templatePath('test.ejs'), this.destinationPath(`${path}/test.ts`), x);
+            }
+
+
             if(x instanceof xmiPackage) {
-                this.generate(`${path}/${x.name}`, x);
+                this._generate(`${path}/${x.name}`, x);
             }
         });
     }
