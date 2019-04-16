38a89a8bef1e5905b9464ccde8b7794d25594fa8
vadimb <vadim.barylo@intapp.com>
2019-04-16 20:05:11 +0300

    add logout support

--- generators/monolith/templates/bootstrap/app/pages/master/app.component.ts
+++ generators/monolith/templates/bootstrap/app/pages/master/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'keycloak-angular';
+  constructor(protected keycloakAngular: KeycloakService) {};
+
+  protected logout() {
+    this.keycloakAngular.logout();
+  }
 }
