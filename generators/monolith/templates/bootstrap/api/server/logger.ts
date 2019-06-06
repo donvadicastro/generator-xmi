b280b8775f68c8a045384e51b3b13731bb0be3e5
vadimb <vadim.barylo@intapp.com>
2019-06-06 23:06:32 +0300

    use local cache for exchanges

--- generators/monolith/templates/bootstrap/api/server/logger.ts
+++ generators/monolith/templates/bootstrap/api/server/logger.ts
@@ -3,6 +3,7 @@ import pino from 'pino';
 const l = pino({
   name: process.env.APP_ID || 'application-id',
   level: process.env.LOG_LEVEL || 'debug',
+  prettyPrint: { forceColor: true }
 });
 
 export default l;
