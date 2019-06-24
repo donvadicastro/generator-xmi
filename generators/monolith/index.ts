48b064af028200af624721685428b5b399b8a987
vadimb <vadim.barylo@intapp.com>
2019-06-24 21:22:46 +0300

    add tests base for classes

--- generators/monolith/index.ts
+++ generators/monolith/index.ts
@@ -133,6 +133,7 @@ export class XmiGenerator extends XmiGeneratorBase {
                 const interfaceFileName = this.destinationPath(`${path}/contracts/${x.name}.ts`);
                 const baseClassFileName = this.destinationPath(`${path}/components/generated/${x.name}.generated.ts`);
                 const classFileName = this.destinationPath(`${path}/components/${x.name}.ts`);
+                const classTestFileName = this.destinationPath(`${path}/components/${x.name}.test.ts`);
                 const apiRouterFileName = this.destinationPath(`${this.options.destination}/api/server/routes/${localPath}/${x.name}.router.ts`);
                 const appComponentRootPath = this.destinationPath(`${this.options.destination}/app/pages/administration/${localPath}`);
 
@@ -148,6 +149,10 @@ export class XmiGenerator extends XmiGeneratorBase {
                 if(!this.fs.exists(classFileName)) {
                     this.fs.copyTpl(this.templatePath('xmiClass.ejs'), classFileName, options);
                     this.generatedFiles.push(classFileName);
+                }
+
+                if(!this.fs.exists(classTestFileName)) {
+                    this.fs.copyTpl(this.templatePath('xmiClass.test.ejs'), classTestFileName, options);
                 }
 
                 //api
