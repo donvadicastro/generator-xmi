9319aa1f67948c398fd12546e8beae2f94ea91a1
vadimb <vadim.barylo@intapp.com>
2019-07-07 22:55:55 +0300

    add checkbox support in e2e

--- generators/monolith/templates/bootstrap/e2e/controls/dropdown.ts
+++ generators/monolith/templates/bootstrap/e2e/controls/dropdown.ts
@@ -3,5 +3,7 @@ import {by} from "protractor";
 
 export class DropdownControl extends BaseControl {
     async setValue(value: string): Promise<void> {
+        await this.element.click();
+        await this.element.element(by.cssContainingText('.ui-dropdown-item span', value)).click();
     }
 }
