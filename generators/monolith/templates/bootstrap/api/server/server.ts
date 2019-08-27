3d02b90ad90d10c98c45b139be000d0b85c1ed89
vadimb <vadim.barylo@intapp.com>
2019-08-27 23:56:28 +0300

    move config into separate file

--- generators/monolith/templates/bootstrap/api/server/server.ts
+++ generators/monolith/templates/bootstrap/api/server/server.ts
@@ -14,15 +14,15 @@ const solutionRoot = path.normalize(root + '/..');
 const pino = require('express-pino-logger')({ logger: logger });
 const app = express();
 const cors = require('cors');
-const pkg = require('../../package.json');
 const ormConfig = require('../../ormconfig.json');
+const env = process.env;
 
 export default class ExpressServer {
   constructor() {
     app.set('appPath', root + 'client');
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cookieParser(process.env.SESSION_SECRET));
+    app.use(cookieParser(env.SESSION_SECRET));
     app.use(express.static(`${root}/public`));
     app.use(cors());
     app.use(pino);
@@ -33,13 +33,14 @@ export default class ExpressServer {
     return this;
   }
 
-  async listen(port: number = parseInt(process.env.PORT || '3000')): Promise<Application> {
+  async listen(port: number = parseInt(env.PORT || '3000')): Promise<Application> {
     //fix ORM entities path
     ormConfig.entities = ormConfig.entities.map((x: string) => `${solutionRoot}/${x}`);
     ormConfig.subscribers = ormConfig.subscribers.map((x: string) => `${solutionRoot}/${x}`);
 
-    const welcome = (port: number) => () => logger.info(`up and running in ${process.env.NODE_ENV || 'development'} @: ${os.hostname() } on port: ${port}}`);
-    const connection = await createConnection({...pkg.db, ...ormConfig});
+    const welcome = (port: number) => () => logger.info(`up and running in ${env.NODE_ENV || 'development'} @: ${os.hostname() } on port: ${port}}`);
+    const db = { type: env.DB_TYPE, host: env.DB_HOST, port: env.DB_PORT, username: env.DB_USERNAME, password: env.DB_PASSWORD, database: env.DB_NAME };
+    const connection = await createConnection({...db, ...ormConfig});
 
     http.createServer(app).listen(port, welcome(port));
     return app;
