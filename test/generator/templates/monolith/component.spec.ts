0907fae05b9264a140a60a6f11f922add79d467a
vadimb <vadim.barilo@gmail.com>
2020-06-15 18:39:00 +0300

    add tests

--- test/generator/templates/monolith/component.spec.ts
+++ test/generator/templates/monolith/component.spec.ts
@@ -12,16 +12,30 @@ describe('Generators', () => {
         describe('Monolith', () => {
             describe('Component', () => {
                 const dir = path.join(__dirname, '../../../../generators/monolith/templates/partial/component');
+                const data = readJSONSync('test/data/project11_activity_condition.json');
+                const parser = new XmiParser(data);
+
+                parser.parse();
+
+                const pkg = <xmiPackage>parser.packge;
+                const classes: xmiPackage = <xmiPackage>(<xmiPackage>pkg.children[0]).children[3];
+                const classA = <xmiClass>classes.children[0];
+
+                it('check methods', async () => {
+                    const content = await ejs.renderFile(path.join(dir, 'operations.ejs'), {entity: classA});
+
+                    expect(content.normalizeSpace()).toBe(`
+                        /** 
+                         * afn1 action. 
+                        */ 
+                        async afn1(): void | null { 
+                            return await null; 
+                        }
+                    `.normalizeSpace());
+
+                });
 
                 it('check conditions', async () => {
-                    const data = readJSONSync('test/data/project11_activity_condition.json');
-                    const parser = new XmiParser(data);
-
-                    parser.parse();
-
-                    const pkg = <xmiPackage>parser.packge;
-                    const classes: xmiPackage = <xmiPackage>(<xmiPackage>pkg.children[0]).children[3];
-                    const classA = <xmiClass>classes.children[0];
                     const content = await ejs.renderFile(path.join(dir, 'conditions.ejs'), {entity: classA});
 
                     expect(content.normalizeSpace()).toBe(`
