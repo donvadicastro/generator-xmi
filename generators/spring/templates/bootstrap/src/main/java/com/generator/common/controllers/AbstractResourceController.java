37299aa6bd4be95c1337944e66448152171aad8b
vadimb <vadim.barylo@intapp.com>
2021-06-10 21:58:49 +0300

    consistency in API

--- generators/spring/templates/bootstrap/src/main/java/com/generator/common/controllers/AbstractResourceController.java
+++ generators/spring/templates/bootstrap/src/main/java/com/generator/common/controllers/AbstractResourceController.java
@@ -1,5 +1,6 @@
 package com.generator.common.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,21 +13,25 @@ public abstract class AbstractResourceController<T> {
     private final PagingAndSortingRepository<T, Integer> repository;
 
     @GetMapping
+    @ApiOperation(value = "get all", notes = "Get all entities")
     public ResponseEntity<Iterable<T>> getAll() { return new ResponseEntity<>(repository.findAll(), HttpStatus.OK); }
 
     @GetMapping({"/{id}"})
+    @ApiOperation(value = "get by id", notes = "Get single entity by id")
     public ResponseEntity<T> get(@PathVariable Integer id) {
         return repository.findById(id).map(t -> new ResponseEntity<>(t, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping()
+    @ApiOperation(value = "create new", notes = "Create new entity")
     public ResponseEntity<T> create(@RequestBody T entity) {
         //TODO: add support location header
         return new ResponseEntity<>(repository.save(entity), HttpStatus.CREATED);
     }
 
     @PutMapping({"/{id}"})
+    @ApiOperation(value = "update by id", notes = "Update entity by id")
     public ResponseEntity<T> update(@PathVariable("id") Integer id, @RequestBody T entity) {
         return repository.findById(id).map(t -> {
             BeanUtils.copyProperties(entity, t, "id");
@@ -35,6 +40,7 @@ public abstract class AbstractResourceController<T> {
     }
 
     @DeleteMapping({"/{id}"})
+    @ApiOperation(value = "delete by id", notes = "Delete entity by id")
     public ResponseEntity delete(@PathVariable("id") Integer id) {
         return repository.findById(id).map(t -> {
             repository.deleteById(id);
