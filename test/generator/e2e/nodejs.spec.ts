0a08f8d0b8198b635486a5758a93515e2a2569e0
vadimb <vadim.barylo@intapp.com>
2021-07-30 22:18:39 +0300

    base tests with testcontainers

--- test/generator/e2e/nodejs.spec.ts
+++ test/generator/e2e/nodejs.spec.ts
@@ -1,46 +1,79 @@
 import {StartedTestContainer} from "testcontainers/dist/test-container";
 import * as path from "path";
 import {GenericContainer} from "testcontainers";
+import anything = jasmine.anything;
+
+const request = require('supertest');
 
 describe('nodejs generator E2E tests', () => {
-    let container: StartedTestContainer;
+    let appContainer: StartedTestContainer;
+    let postgresContainer: StartedTestContainer;
 
     beforeAll(async () => {
-        jest.setTimeout(240000);
+        jest.setTimeout(360000);
 
-        console.log('building image');
+        postgresContainer = await new GenericContainer("postgres")
+            .withExposedPorts(5432)
+            .withEnv("POSTGRES_USER", "test")
+            .withEnv("POSTGRES_PASSWORD", "test")
+            .withEnv("POSTGRES_DB", "test")
+            .start();
+
         const buildContainer = await GenericContainer.fromDockerfile(path.resolve('./test/generator/e2e'))
             .build();
 
-        container = await buildContainer
-            .withCopyFileToContainer(path.resolve(__dirname, '../../../test/data/fixtures.xml'), "/fixtures.xml")
+        appContainer = await buildContainer
+            .withCopyFileToContainer(path.resolve(__dirname, '../../../test/data/fixtures.xml'), "/generated/fixtures.xml")
+            .withExposedPorts(3000, 4200)
+            .withEnv('DB_HOST', postgresContainer.getIpAddress('bridge'))
             .start();
     });
 
-    // beforeAll(async () => {
-    //     jest.setTimeout(1200000);
-    //
-    //     container = await new GenericContainer("outrigger/yeoman")
-    //         .withName("xmi-nodejs")
-    //         .withBindMount(path.resolve(__dirname, '../../../package.json'), "/generated/package.json")
-    //         .withBindMount(path.resolve(__dirname, '../../../generators'), "/generated/generators")
-    //         .withBindMount(path.resolve(__dirname, '../../../test/data'), "/generated/data")
-    //         .start();
-    // });
-    //
-    // beforeAll(async () => {
-    //     await container.exec(["npm", "install"]);
-    //     //await container.exec(["npm", "link"]);
-    // });
+    beforeAll(async () => {
+        expect(await appContainer.exec(["yo", "--generators"]))
+            .toEqual({exitCode: 0, output: 'Available Generators:\n\n  xmi\n    microservices\n    nodejs\n    spring\n    monolith\n'});
 
-    afterAll(async () => {
-        await container.stop();
+        expect(await appContainer.exec(["yo", "xmi", "fixtures.xml", "--type=nodejs", "--destination=."]))
+            .toEqual({exitCode: 0, output: anything()});
     });
 
     describe('API server', () => {
-        it('should generate successfully', async () => {
-            await container.exec(["yo", "xmi", "data/fixtures.xml --type=nodejs"]);
-            expect(await container.exec(["ls"])).toBe('');
+        let req: any;
+
+        beforeAll(async () => {
+            await appContainer.exec(["npm", "run", "api:start:forever"]);
+            req = request(`http://localhost:${appContainer.getMappedPort(3000)}`);
+        });
+
+        it('should start API server successfully', (done) => {
+            req.get('/api-explorer').expect(200, done);
+        });
+
+        describe('x1-simple-independent-classes -> vehicle', () => {
+            describe('should support GET method', () => {
+                it('existing', (done) => {
+                    req.get('/api/v1/class-diagrams/x1-simple-independent-classes/vehicle').expect(200, done);
+                });
+
+                it('not found', (done) => {
+                    req.get('/api/v1/class-diagrams/x1-simple-independent-classes/vehicle/0').expect(404, done);
+                });
+            });
+
+            it('should support POST method', () => {
+            });
+        });
+    });
+
+    describe('APP server', () => {
+        beforeAll(async () => {
+            await appContainer.exec(["npm", "run", "app:start:forever"]);
+            await new Promise(x => setTimeout(x, 10000));
+        });
+
+        it('should start APP server successfully', (done) => {
+            const req = request(`http://localhost:${appContainer.getMappedPort(4200)}`);
+            req.get('/administration/classDiagrams/x1SimpleIndependentClasses/person').expect(200, done);
         });
     });
 });
