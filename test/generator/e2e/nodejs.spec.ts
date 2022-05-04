8c4e223bd7f8cc9b051bf9f38ecc2ab7b9917800
Vadym <vadim.barylo@intapp.com>
2022-05-04 18:08:08 +0300

    run tests

--- test/generator/e2e/nodejs.spec.ts
+++ test/generator/e2e/nodejs.spec.ts
@@ -5,30 +5,43 @@ import {GenericContainer} from "testcontainers";
 describe('nodejs generator E2E tests', () => {
     let container: StartedTestContainer;
 
-    beforeAll(async () => {
-        jest.setTimeout(240000);
+    beforeAll((done) => {
+        jest.setTimeout(300_000);
 
         console.log('building image');
-        const buildContainer = await GenericContainer.fromDockerfile(path.resolve('./test/generator/e2e'))
-            .build();
+        GenericContainer.fromDockerfile(path.resolve('./test/generator/e2e')).build().then(x => {
+            console.log('starting image');
+            return x.withBindMount(path.resolve(__dirname, '../../../test/data'), "/generated/data").start();
+        }).then(x => {
+            console.log('started', x.getName());
+            container = x;
 
-        container = await buildContainer.start();
+            done();
+        });
     });
 
     // beforeAll(async () => {
     //     jest.setTimeout(1200000);
     //
-    //     container = await new GenericContainer("outrigger/yeoman")
+    //     container = await new GenericContainer("node:alpine")
     //         .withName("xmi-nodejs")
-    //         .withBindMount(path.resolve(__dirname, '../../../package.json'), "/generated/package.json")
-    //         .withBindMount(path.resolve(__dirname, '../../../generators'), "/generated/generators")
-    //         .withBindMount(path.resolve(__dirname, '../../../test/data'), "/generated/data")
+    //         .withBindMount(path.resolve(__dirname, '../../../package.json'), "/package.json")
+    //         .withBindMount(path.resolve(__dirname, '../../../generators'), "/generators")
+    //         .withBindMount(path.resolve(__dirname, '../../../node_modules'), "/node_modules")
+    //         .withBindMount(path.resolve(__dirname, '../../../src'), "/src")
+    //         .withBindMount(path.resolve(__dirname, '../../../test/data'), "/data")
+    //         .withCmd(["tail", "-f", "/dev/null"])
     //         .start();
-    // });
     //
+    //     await container.exec(["npm", "install", "yo", "rimraf", "--global"]);
+    //     await container.exec(["npm", "link"]);
+    //     await container.exec(["bash", "-c", "'chmod g+rwx /root /root/.config /root/.config/insight-nodejs'"]);
+    // });
+
     // beforeAll(async () => {
-    //     await container.exec(["npm", "install"]);
-    //     //await container.exec(["npm", "link"]);
+    //     // await container.exec(["npm", "install"]);
+    //     await container.exec(["npm", "link"]);
+    //     await container.exec(["tail", "-f", "/dev/null"]); //to keep running
     // });
 
     afterAll(async () => {
@@ -36,9 +49,13 @@ describe('nodejs generator E2E tests', () => {
     });
 
     describe('API server', () => {
-        it('should generate successfully', async () => {
-            await container.exec(["yo", "xmi", "data/fixtures.xml --type=nodejs"]);
-            expect(await container.exec(["ls"])).toBe('');
+        it('should generate successfully', (done) => {
+            container.exec(["yo", "xmi", "data/fixtures.xml", "--type=nodejs"])
+                .then(x => container.exec(["ls"]))
+                .then(x => {
+                    expect(x).toEqual({ "exitCode": 0, "output": "data\ndist\n" });
+                    done();
+                });
         });
     });
 });
