0d992ca1e097a3793e873854fb8daedf0a88cf06
Vadym <vadim.barylo@intapp.com>
2022-06-09 17:51:17 +0300

    fix CI

--- generators/nodejs/templates/bootstrap/build.ts
+++ generators/nodejs/templates/bootstrap/build.ts
@@ -1,16 +1,16 @@
-import * as s from 'shelljs';
+const shell = require('shelljs');
 const config = require('./tsconfig.json');
 const outDir = config.compilerOptions.outDir;
 
-s.rm('-rf', outDir);
-s.mkdir(outDir);
-s.mkdir('-p', `${outDir}/api/public`);
+shell.rm('-rf', outDir);
+shell.mkdir(outDir);
+shell.mkdir('-p', `${outDir}/api/public`);
 
-s.cp('.env', `${outDir}/.env`);
-s.cp('package.json', `${outDir}/package.json`);
-s.cp('ormconfig.json', `${outDir}/ormconfig.json`);
-s.cp('-r', 'api/public', `${outDir}/api`);
-s.mkdir('-p', `${outDir}/api/server/swagger/swagger`);
-s.cp('api/server/swagger/api.yaml', `${outDir}/api/server/swagger/api.yaml`);
+shell.cp('.env', `${outDir}/.env`);
+shell.cp('package.json', `${outDir}/package.json`);
+shell.cp('ormconfig.json', `${outDir}/ormconfig.json`);
+shell.cp('-r', 'api/public', `${outDir}/api`);
+shell.mkdir('-p', `${outDir}/api/server/swagger/swagger`);
+shell.cp('api/server/swagger/api.yaml', `${outDir}/api/server/swagger/api.yaml`);
 
-s.exec(`cd ${outDir} && tsc`);
+shell.exec(`cd ${outDir} && tsc`);
